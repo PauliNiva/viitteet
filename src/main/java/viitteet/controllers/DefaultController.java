6afdd2477b57be166de2af19d23b82569e38a6ac
Pauli Niva <pauli.niva@helsinki.fi>
2015-03-31 07:39:16 +0300

    Hello World

---
+++ src/main/java/viitteet/controllers/DefaultController.java
@@ -0,0 +1,13 @@
+package viitteet.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DefaultController {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}
