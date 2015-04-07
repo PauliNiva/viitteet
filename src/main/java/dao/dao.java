97b67ec88f9c09f9f979e16383b0195f913470d1
ssomero <sonja.somero@helsinki.fi>
2015-04-07 14:12:35 +0300

    Authorit yms poistettu tiedostoista

--- src/main/java/dao/dao.java
+++ src/main/java/dao/dao.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dao;
 
 import java.util.List;
 import viitehallinta.Viite;
 
-/**
- *
- * @author Sonja
- */
 public interface dao {
     void lisaaViite(Viite viite);
     List <Viite> listaaViitteet();
--- src/main/java/io/IOtallennus.java
+++ src/main/java/io/IOtallennus.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package io;
 
 import java.util.List;
 import viitehallinta.Viite;
 
-/**
- *
- * @author Sonja
- */
 public interface IOtallennus {
     boolean kirjoita(List<Viite> viitteet);
     List <Viite> lue();
