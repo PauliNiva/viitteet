a083dcbe294f28ebf32fac46d05c264862ae6b8b
Teemu Ijäs <tijas@cs.helsinki.fi>
2015-04-12 14:05:17 +0300

    k

--- src/test/java/ui/KayttoliittymaTest.java
+++ src/test/java/ui/KayttoliittymaTest.java
@@ -1,4 +1,26 @@
 package ui;
 
+import io.IO;
+import io.KayttoliittymaIO;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import viitehallinta.Viitearkisto;
+
 public class KayttoliittymaTest {
+    
+    IO io;
+    Viitearkisto viitearkisto;
+    
+    @Before
+    public void setUp(){
+        io = new KayttoliittymaIO();
+        
+    }
+    
+    @Test
+    public void getKayttajanValintaTest(){
+        
+    }
+    
 }
