afa1388bcaa814df57a01750b4198c06b7ee9038
Pauli Niva <pauli.niva@helsinki.fi>
2015-04-13 15:01:30 +0300

    siirretty stub testeihin

---
+++ src/test/java/io/StubIO.java
@@ -0,0 +1,43 @@
+package io;
+
+import io.IO;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+public class StubIO implements IO {
+
+    private String[] lines;
+    private int i;
+    private ArrayList<String> prints;
+
+    public StubIO(String... values) {
+        this.lines = values;
+        prints = new ArrayList<String>();
+    }
+
+    public ArrayList<String> getPrints() {
+        return prints;
+    }
+
+    @Override
+    public String lueRivi() {
+        if (i < lines.length) {
+            return lines[i++];
+        }
+        return "";
+    }
+
+    @Override
+    public void tulostaIlmanRivinvaihtoa(String rivi) {
+        System.out.print(rivi);
+        prints.add(rivi);
+    }
+
+    @Override
+    public void tulostaRivi(String rivi) {
+        System.out.println(rivi);
+        prints.add(rivi);
+    }
+
+    
+}
