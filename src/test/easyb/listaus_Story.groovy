82fa64c74078a48e280e9414b1dd566c2c60ef47
Pauli Niva <pauli.niva@helsinki.fi>
2015-04-13 14:44:02 +0300

    easyb done!

--- src/test/easyb/listaus_Story.groovy
+++ src/test/easyb/listaus_Story.groovy
@@ -8,7 +8,7 @@ description 'Käyttäjä voi listata artikkelit'
 scenario 'käyttäjä voi listata artikkelit', {
     given 'lisäämis-toiminto on valittu', {
         testiDao = new FileDao("tyhjätestiviite.txt");
-        io = new StubIO("2");
+        io = new StubIO("2", "4");
         viitearkisto = new Viitearkisto(testiDao);
         kl = new Kayttoliittyma(io, viitearkisto);
         
