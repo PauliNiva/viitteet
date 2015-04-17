cf47f2cd8c28af6dd5e92242a43ab20ea0a9f401
Teemu Ijäs <tijas@cs.helsinki.fi>
2015-04-17 19:18:01 +0300

    Bibtex päivittelyä

--- src/main/java/io/Bibtex.java
+++ src/main/java/io/Bibtex.java
@@ -1,4 +1,3 @@
-
 package io;
 
 import dao.FileDao;
@@ -14,13 +13,17 @@ import viitehallinta.Viitearkisto;
  * Luokka joka luo viitteistä BibTex-muotoisen tiedoston
  */
 public class Bibtex {
+
     private List<Viite> viitteet;
     private IO io;
     private FileDao dao;
+    private String tiedosto;
 
     /**
-     * luonnissa tarvitsemme viitearkiston ja käyttöliittymän
-     * 
+     * luonnissa tarvitsemme viitearkiston ja käyttöliittymän sekä tiedoston,
+     * tiedoston nimeä voisi kysyä käyttäjältä tiedoston luonnin valinnan
+     * yhteydessä
+     *
      * @param viitearkisto
      * @param io
      * @param tiedosto
@@ -28,36 +31,59 @@ public class Bibtex {
     public Bibtex(Viitearkisto viitearkisto, IO io, String tiedosto) {
         this.io = io;
         this.dao = new FileDao(tiedosto, io);
+        this.tiedosto = tiedosto;
         // this.viitteet = viitearkisto.getViitteet sitten kun refaktorointi on valmis
-    }
-    
-    /**
-     *
-     * @throws IOException
-     */
-    public void luoTiedosto() throws IOException{
-        dao.tyhjennaTiedosto();
-        for (int i = 0; i < viitteet.size(); i++) {
-            String viitetyyppi = viitteet.get(i).getViiteTyyppi();
-            if (viitetyyppi.equals("Artikkeli")){
-                lisaaArtikkeliTiedostoon(viitteet.get(i));
-            }
-            if (viitetyyppi.equals("Kirja")){
-                lisaaKirjaTiedostoon(viitteet.get(i));
-            }
-        }
     }
 
     /**
+     * tyhjentää tiedoston aluksi, jonka jälkeen luo tiedoston kutsumalla eri
+     * metodeita erityyppisille Viite-olioille
+     *
+     * @throws IOException
+     */
+    public void luoTiedosto() throws IOException {
+        dao.tyhjennaTiedosto();
+        for (int i = 0; i < viitteet.size(); i++) {
+            String viitetyyppi = viitteet.get(i).getViiteTyyppi();
+            if (viitetyyppi.equals("Artikkeli")) {
+                lisaaArtikkeliTiedostoon(viitteet.get(i));
+            }
+            if (viitetyyppi.equals("Kirja")) {
+                lisaaKirjaTiedostoon(viitteet.get(i));
+            }
+        }
+
+        io.tulostaRivi("Viitteet lisätty " + tiedosto + " tiedostoon");
+    }
+
+    /**
+     * lisää tiedostoon BibTex-muodossa artikkelin tiedot, 
+     * TODO!
+     * ensimmäisen
+     * rivin sisältö on hieman mysteeri mutta oletin sen olevan ID 
+     * TODO!!
+     * artikkelin ulos ottaminen objectina, getclassilla luokan haku ja objectin
+     * castaaminen oikeaksi luokaksi mahdollista!
      *
      * @param artikkeli
      * @throws IOException
      */
     public void lisaaArtikkeliTiedostoon(Viite artikkeli) throws IOException {
         dao.lisaaRiviTiedostoon("@article{" + tarkastaAakkoset(artikkeli.getID()) + ",");
+        dao.lisaaRiviTiedostoon("author = {" + tarkastaAakkoset(artikkeli.getAuthor()) + "},");
+        dao.lisaaRiviTiedostoon("title = {" + tarkastaAakkoset(artikkeli.getTitle()) + "},");
+        //dao.lisaaRiviTiedostoon("journal = {" + tarkastaAakkoset(artikkeli.getJournal()) + "},");
+        //dao.lisaaRiviTiedostoon("volume = {" + tarkastaAakkoset(artikkeli.getVolume()) + "},");
+        //dao.lisaaRiviTiedostoon("number = {" + tarkastaAakkoset(artikkeli.getNumber()) + "},");
+        dao.lisaaRiviTiedostoon("year = {" + artikkeli.getYear() + "},");
+        dao.lisaaRiviTiedostoon("pages = {" + tarkastaAakkoset(artikkeli.getPages()) + "},");
+        dao.lisaaRiviTiedostoon("publisher = {" + tarkastaAakkoset(artikkeli.getPublisher()) + "},");
+        dao.lisaaRiviTiedostoon("}");
+        dao.lisaaRiviTiedostoon("");
     }
 
     /**
+     * TODO! samaan tyyliin kuin artikkelin lisäys
      *
      * @param kirja
      */
@@ -66,14 +92,16 @@ public class Bibtex {
     }
 
     /**
-     * tarkastaa ja muuttaa parametrina annetun merkkijonon ääkköset bibitex-muotoon
+     * TODO!! tarkastaa ja muuttaa parametrina annetun merkkijonon ääkköset
+     * bibitex-muotoon
+     *
      * @param rivi
      * @return
      */
     public String tarkastaAakkoset(String rivi) {
-        String korjattuRivi = "";
-        
+        String korjattuRivi = rivi;
+
         return korjattuRivi;
     }
-    
+
 }
